50626e611f34bea456a81a7dd16a1e72381e749f
cyber <34314698+etozhecyber@users.noreply.github.com>
2026-05-02 20:26:13 +0200

    add github pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,16 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
-      plugins: [react()],
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
+        base: '/CharactersGenerator/',
+        server: {
+            port: 3000,
+            host: '0.0.0.0',
+        },
+        plugins: [react()],
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, '.'),
+            }
         }
-      }
     };
 });
